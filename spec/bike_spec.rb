4c5c98b8ffaede16da65027b0ff84a057b3bf5ae
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-22 12:20:13 +0000

    Added release_bike method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,10 +1,6 @@
 require 'bike'
 
 describe Bike do
-  subject { Bike.new }
-
-  it { should respond_to(:working?) }
-
   it 'responds to :working?' do
     expect(subject.working?).to eq true
   end
