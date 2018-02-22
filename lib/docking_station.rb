5e8ea664168e5db9f7dc23d7c8042639d0092192
Thomas Duffy <tomjamesduffy@gmail.com>
2018-02-22 12:09:55 +0000

    Excercise 9

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,33 +1,7 @@
-require 'bike'
-
 class DockingStation
   def release_bike
-    bike = Bike.new
+  
   end
-
-
 end
 
 
-#   attr_reader :bike
-#
-#   def release_bike
-#     Bike.new
-#   end
-#
-#   def dock(bike)
-#     @bike = bike
-#   end
-#
-#   def bike_available?
-#     if !@bike == true
-#       raise "Sorry, no bike in the dock"
-#     end
-#   end
-#
-# end
-#
-# class Bike
-#   def working?
-#     true
-#   end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,13 +1,8 @@
 require 'bike'
 
 describe Bike do
-  subject { Bike.new }
-
-  it { should respond_to(:working?) }
-
   it 'responds to :working?' do
-    expect(subject.working?).to eq true
+    expect(subject.working?).to eq(true)
   end
-
-
 end
+
