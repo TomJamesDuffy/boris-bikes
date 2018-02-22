df504140af83d42d4e75a858818722f99daa0bde
Thomas Duffy <tomjamesduffy@gmail.com>
2018-02-22 12:30:59 +0000

    Excercise 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,13 @@
-class DockingStation
-  def release_bike
+require 'bike'
 
+class DockingStation
+  attr_reader :bike
+
+  def release_bike
+    Bike.new
+  end
+
+  def dock_bike(bike)
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it 'responds to :release_bike' do
@@ -7,5 +8,9 @@ describe DockingStation do
   
   it ':release_bike returns a new bike' do
     expect(subject.release_bike).to be_instance_of(Bike)
+  end
+
+  it ':dock_bike adds a bike' do
+    expect(subject.dock_bike(Bike.new)).to be_instance_of(Bike)
   end
 end
